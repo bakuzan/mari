a5f40470eb76d82fed3f3238a182643f8f9fd532
bakuzan <imannoyed4days@hotmail.co.uk>
2018-06-15 21:07:56 +0100

    working on understanding recursive backtracker...

--- generation/generator.py
+++ generation/generator.py
@@ -1,4 +1,4 @@
-import random
+from random import randrange, sample
 
 NOT_VISITED = 0
 VISITED_PASSAGE = ' '
@@ -10,7 +10,7 @@ OPPOSITE = {E: W, W: E, N: S, S: N}
 
 
 def carve_passages_from(x, y, grid):
-    directions = random.sample(compass_directions, len(compass_directions))
+    directions = sample(compass_directions, len(compass_directions))
     for d in directions:
         next_x, next_y = x + DX[d], y + DY[d]
 
@@ -25,7 +25,7 @@ def carve_passages_from(x, y, grid):
 def loop_carve_passages_from(x, y, grid):
     height = len(grid) - 1
     width = len(grid[0]) - 1
-    directions = random.sample(compass_directions, len(compass_directions))
+    directions = sample(compass_directions, len(compass_directions))
     stack = [(x, y, directions[:])]
     while len(stack) != 0:
         print(stack)
@@ -39,6 +39,68 @@ def loop_carve_passages_from(x, y, grid):
                 stack.append((nx, ny, directions[:]))
         stack.pop(0)
     return grid
+
+
+class MazeGenerator:
+    """
+    Generator object for maze
+    """
+    PASSAGE = 0
+    WALL = 1
+
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+        self.hMax = (2 * h) + 1
+        self.wMax = (2 * w) + 1
+
+    def generate(self):
+        g = [[self.WALL for c in range(self.wMax)] for r in range(self.hMax)]
+        grid = g
+
+        # random starting co-ords from 1 to index, multiples of 2
+        c_row = randrange(1, self.hMax, 2)
+        c_col = randrange(1, self.wMax, 2)
+        stack = [(c_row, c_col)]
+        grid[c_row][c_col] = self.PASSAGE
+
+        while grid:
+            (c_row, c_col) = stack[-1]
+            neighbours = self._get_neighbours(c_row, c_col, grid)
+
+            if len(neighbours) == 0:
+                # drop the current item from the stack
+                stack = stack[:-1]
+            else:
+                # carve passage in neighbour
+                n_row, n_col = neighbours[0]
+                grid[n_row][n_col] = self.PASSAGE
+                grid[(n_row + c_row) // 2][(n_col + c_col) // 2] = self.PASSAGE
+                stack += [(n_row, n_col)]
+        
+        return grid
+
+    """
+    internals
+    """
+
+    def _get_neighbours(self, x, y, grid):
+        """
+        get 'neighbouring' cells in a random order
+        """
+        n_list = []
+
+        if x > 1 and grid[x - 2][y] == self.WALL:
+            n_list.append((x - 2, y))
+        if x < self.hMax - 2 and grid[x + 2][y] == self.WALL:
+            n_list.append((x + 2, y))
+        if y > 1 and grid[x][y - 2] == self.WALL:
+            n_list.append((x, y - 2))
+        if y < self.wMax - 2 and grid[x][y + 2] == self.WALL:
+            n_list.append((x, y + 2))
+
+        return sample(n_list, len(n_list))
+
 
 
 def generate(width, height):
@@ -59,4 +121,12 @@ def generate(width, height):
 
 
 if __name__ == "__main__":
-    generate(37, 23)
+    # generate(37, 23)
+    m = MazeGenerator(37,23)
+    maze = m.generate()
+    display = []
+    for y, _ in enumerate(maze):
+        for x, _ in enumerate(maze[y]):
+            display.append("#" if maze[y][x] == True else ' ')
+        display.append('\n')
+    print("".join(display))
