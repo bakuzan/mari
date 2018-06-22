bb208709620fd72f9bdf650c3c48220459a63d75
bakuzan <imannoyed4days@hotmail.co.uk>
2018-06-22 15:16:52 +0100

    implemented line of sight.

--- maze/maze.py
+++ maze/maze.py
@@ -7,7 +7,7 @@ from maze.point import Point
 from maze.character.mari import Mari
 from maze.character.troll import Troll
 from maze.generator import MazeGenerator
-from maze.utf_render import render_square
+from maze.renderer import Renderer
 from path_finding import a_star_search
 from viewer.viewer import Viewer
 
@@ -15,7 +15,7 @@ from viewer.viewer import Viewer
 class Maze:
     """
     Holds all the information about the current maze
-    Provides method to interact with the game. 
+    Provides method to interact with the game.
     """
 
     def __init__(self, w, h, troll_count=3):
@@ -61,10 +61,13 @@ class Maze:
 
     def render(self):
         player_location = self.player.get_location()
+        render_factory = Renderer(self.layout, player_location)
         display = []
+
         for row_i, row in enumerate(self.layout):
             for col_i, col in enumerate(row):
                 current_point = Point(col_i, row_i)
+
                 if current_point == player_location:
                     display.append(self.player.render())
                 elif current_point in [t.get_location() for t in self.trolls]:
@@ -72,15 +75,16 @@ class Maze:
                         t for t in self.trolls if current_point == t.get_location()]
                     troll = trolls[0]
                     if col != constants.maze_point_wall:
-                        display.append(troll.render())
+                        display.append(
+                            render_factory.render_square(current_point, troll.render()))
                     else:
                         display.append(
-                            render_square(self.layout, current_point, col))
+                            render_factory.render_square(current_point, col))
                         self.trolls = [
                             t for t in self.trolls if t.id != troll.id]
                 else:
                     display.append(
-                        render_square(self.layout, current_point, col))
+                        render_factory.render_square(current_point, col))
             display.append('\n')
 
         self.__window.update("".join(display))
---
+++ maze/renderer.py
@@ -0,0 +1,101 @@
+from maze.constants import translations, maze_point_wall, UP, LEFT, DOWN, RIGHT
+
+ALL_DIRECTIONS = 4
+SIGHT_RANGE = 5
+
+DARK_SHADE = u'\u2593'
+BOX_SQUARE = u'\u25A1'
+
+BOX_TOP_LEFT = u'\u2554'
+BOX_TOP_RIGHT = u'\u2557'
+BOX_BOTTOM_LEFT = u'\u255a'
+BOX_BOTTOM_RIGHT = u'\u255d'
+
+BOX_HORIZONTAL = u'\u2550'
+BOX_VERTICAL = u'\u2551'
+
+BOX_CROSS = u'\u256c'
+BOX_VERTICAL_RIGHT = u'\u2560'
+BOX_VERTICAL_LEFT = u'\u2563'
+BOX_HORIZONTAL_UP = u'\u2569'
+BOX_HORIZONTAL_DOWN = u'\u2566'
+
+BOX_CAP_TOP = u'\u2565'
+BOX_CAP_BOTTOM = u'\u2568'
+BOX_CAP_LEFT = u'\u2552'
+BOX_CAP_RIGHT = u'\u2555'
+
+
+class Renderer:
+    """
+    Class that works out what to render in a given square
+    """
+
+    def __init__(self, maze, player_location):
+        self.__maze = maze
+        self.__player_location = player_location
+
+    def render_square(self, current, sq):
+        if not self._is_within_line_of_sight(current):
+            return DARK_SHADE
+
+        if sq != maze_point_wall:
+            return sq
+
+        directions = self._get_neighbouring_wall_directions(current)
+        if len(directions) == ALL_DIRECTIONS:
+            return BOX_CROSS
+        elif set([UP, DOWN, RIGHT]) == directions:
+            return BOX_VERTICAL_RIGHT
+        elif set([UP, DOWN, LEFT]) == directions:
+            return BOX_VERTICAL_LEFT
+        elif set([UP, LEFT, RIGHT]) == directions:
+            return BOX_HORIZONTAL_UP
+        elif set([DOWN, LEFT, RIGHT]) == directions:
+            return BOX_HORIZONTAL_DOWN
+        elif set([UP, DOWN]) == directions:
+            return BOX_VERTICAL
+        elif set([LEFT, RIGHT]) == directions:
+            return BOX_HORIZONTAL
+        elif set([DOWN, RIGHT]) == directions:
+            return BOX_TOP_LEFT
+        elif set([UP, RIGHT]) == directions:
+            return BOX_BOTTOM_LEFT
+        elif set([DOWN, LEFT]) == directions:
+            return BOX_TOP_RIGHT
+        elif set([UP, LEFT]) == directions:
+            return BOX_BOTTOM_RIGHT
+        elif set([UP]) == directions:
+            return BOX_CAP_BOTTOM
+        elif set([DOWN]) == directions:
+            return BOX_CAP_TOP
+        elif set([LEFT]) == directions:
+            return BOX_CAP_RIGHT
+        elif set([RIGHT]) == directions:
+            return BOX_CAP_LEFT
+        else:
+            return BOX_SQUARE
+
+    """
+    internals
+    """
+
+    def _is_within_line_of_sight(self, current):
+        print(current, self.__player_location)
+        px, py = self.__player_location
+        cx, cy = current
+        return abs(cx-px) <= SIGHT_RANGE and abs(cy-py) <= SIGHT_RANGE
+
+    def _get_neighbouring_wall_directions(self, current):
+        neighbour_directions = set()
+        h = len(self.__maze) - 1
+        w = len(self.__maze[0]) - 1
+        cx, cy = current
+
+        for k, (tx, ty) in translations.items():
+            nx = cx + tx
+            ny = cy + ty
+            if nx >= 0 and nx <= w and ny >= 0 and ny <= h:
+                if self.__maze[ny][nx] == maze_point_wall:
+                    neighbour_directions.add(k)
+        return neighbour_directions
